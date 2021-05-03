06826a90f896e87d2e7ede0a042ac48e392f12c0
pink-tan <pink.tan.tank@gmail.com>
2021-05-03 23:45:49 +0900

    初期登録

---
+++ ReinoKaunta/ReinoForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace ReinoKaunta
+{
+    partial class ReinoForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aname = new System.Windows.Forms.Label();
+            this.dname = new System.Windows.Forms.Label();
+            this.cname = new System.Windows.Forms.Label();
+            this.bname = new System.Windows.Forms.Label();
+            this.ascore = new System.Windows.Forms.Label();
+            this.dscore = new System.Windows.Forms.Label();
+            this.cscore = new System.Windows.Forms.Label();
+            this.bscore = new System.Windows.Forms.Label();
+            this.ainc = new System.Windows.Forms.Button();
+            this.adsc = new System.Windows.Forms.Button();
+            this.ddsc = new System.Windows.Forms.Button();
+            this.dinc = new System.Windows.Forms.Button();
+            this.cdsc = new System.Windows.Forms.Button();
+            this.cinc = new System.Windows.Forms.Button();
+            this.bdsc = new System.Windows.Forms.Button();
+            this.binc = new System.Windows.Forms.Button();
+            this.CaptureLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aname
+            // 
+            this.aname.AutoSize = true;
+            this.aname.BackColor = System.Drawing.Color.White;
+            this.aname.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.aname.ForeColor = System.Drawing.Color.Red;
+            this.aname.Location = new System.Drawing.Point(12, 9);
+            this.aname.Name = "aname";
+            this.aname.Size = new System.Drawing.Size(22, 19);
+            this.aname.TabIndex = 0;
+            this.aname.Text = "A";
+            // 
+            // dname
+            // 
+            this.dname.AutoSize = true;
+            this.dname.BackColor = System.Drawing.Color.White;
+            this.dname.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.dname.Location = new System.Drawing.Point(121, 9);
+            this.dname.Name = "dname";
+            this.dname.Size = new System.Drawing.Size(22, 19);
+            this.dname.TabIndex = 1;
+            this.dname.Text = "D";
+            // 
+            // cname
+            // 
+            this.cname.AutoSize = true;
+            this.cname.BackColor = System.Drawing.Color.White;
+            this.cname.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cname.ForeColor = System.Drawing.Color.Olive;
+            this.cname.Location = new System.Drawing.Point(83, 9);
+            this.cname.Name = "cname";
+            this.cname.Size = new System.Drawing.Size(23, 19);
+            this.cname.TabIndex = 2;
+            this.cname.Text = "C";
+            // 
+            // bname
+            // 
+            this.bname.AutoSize = true;
+            this.bname.BackColor = System.Drawing.Color.White;
+            this.bname.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bname.ForeColor = System.Drawing.Color.Green;
+            this.bname.Location = new System.Drawing.Point(47, 9);
+            this.bname.Name = "bname";
+            this.bname.Size = new System.Drawing.Size(22, 19);
+            this.bname.TabIndex = 3;
+            this.bname.Text = "B";
+            // 
+            // ascore
+            // 
+            this.ascore.AutoSize = true;
+            this.ascore.BackColor = System.Drawing.Color.White;
+            this.ascore.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ascore.ForeColor = System.Drawing.Color.Red;
+            this.ascore.Location = new System.Drawing.Point(12, 32);
+            this.ascore.Name = "ascore";
+            this.ascore.Size = new System.Drawing.Size(20, 19);
+            this.ascore.TabIndex = 4;
+            this.ascore.Text = "0";
+            // 
+            // dscore
+            // 
+            this.dscore.AutoSize = true;
+            this.dscore.BackColor = System.Drawing.Color.White;
+            this.dscore.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dscore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.dscore.Location = new System.Drawing.Point(121, 32);
+            this.dscore.Name = "dscore";
+            this.dscore.Size = new System.Drawing.Size(20, 19);
+            this.dscore.TabIndex = 5;
+            this.dscore.Text = "0";
+            // 
+            // cscore
+            // 
+            this.cscore.AutoSize = true;
+            this.cscore.BackColor = System.Drawing.Color.White;
+            this.cscore.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cscore.ForeColor = System.Drawing.Color.Olive;
+            this.cscore.Location = new System.Drawing.Point(83, 32);
+            this.cscore.Name = "cscore";
+            this.cscore.Size = new System.Drawing.Size(20, 19);
+            this.cscore.TabIndex = 6;
+            this.cscore.Text = "0";
+            // 
+            // bscore
+            // 
+            this.bscore.AutoSize = true;
+            this.bscore.BackColor = System.Drawing.Color.White;
+            this.bscore.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bscore.ForeColor = System.Drawing.Color.Green;
+            this.bscore.Location = new System.Drawing.Point(47, 32);
+            this.bscore.Name = "bscore";
+            this.bscore.Size = new System.Drawing.Size(20, 19);
+            this.bscore.TabIndex = 7;
+            this.bscore.Text = "0";
+            // 
+            // ainc
+            // 
+            this.ainc.Location = new System.Drawing.Point(13, 56);
+            this.ainc.Name = "ainc";
+            this.ainc.Size = new System.Drawing.Size(13, 23);
+            this.ainc.TabIndex = 8;
+            this.ainc.Text = "+";
+            this.ainc.UseVisualStyleBackColor = true;
+            this.ainc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // adsc
+            // 
+            this.adsc.Location = new System.Drawing.Point(13, 85);
+            this.adsc.Name = "adsc";
+            this.adsc.Size = new System.Drawing.Size(13, 23);
+            this.adsc.TabIndex = 9;
+            this.adsc.Text = "-";
+            this.adsc.UseVisualStyleBackColor = true;
+            this.adsc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // ddsc
+            // 
+            this.ddsc.Location = new System.Drawing.Point(125, 85);
+            this.ddsc.Name = "ddsc";
+            this.ddsc.Size = new System.Drawing.Size(13, 23);
+            this.ddsc.TabIndex = 11;
+            this.ddsc.Text = "-";
+            this.ddsc.UseVisualStyleBackColor = true;
+            this.ddsc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // dinc
+            // 
+            this.dinc.Location = new System.Drawing.Point(125, 56);
+            this.dinc.Name = "dinc";
+            this.dinc.Size = new System.Drawing.Size(13, 23);
+            this.dinc.TabIndex = 10;
+            this.dinc.Text = "+";
+            this.dinc.UseVisualStyleBackColor = true;
+            this.dinc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // cdsc
+            // 
+            this.cdsc.Location = new System.Drawing.Point(87, 85);
+            this.cdsc.Name = "cdsc";
+            this.cdsc.Size = new System.Drawing.Size(13, 23);
+            this.cdsc.TabIndex = 13;
+            this.cdsc.Text = "-";
+            this.cdsc.UseVisualStyleBackColor = true;
+            this.cdsc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // cinc
+            // 
+            this.cinc.Location = new System.Drawing.Point(87, 56);
+            this.cinc.Name = "cinc";
+            this.cinc.Size = new System.Drawing.Size(13, 23);
+            this.cinc.TabIndex = 12;
+            this.cinc.Text = "+";
+            this.cinc.UseVisualStyleBackColor = true;
+            this.cinc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // bdsc
+            // 
+            this.bdsc.Location = new System.Drawing.Point(51, 85);
+            this.bdsc.Name = "bdsc";
+            this.bdsc.Size = new System.Drawing.Size(13, 23);
+            this.bdsc.TabIndex = 15;
+            this.bdsc.Text = "-";
+            this.bdsc.UseVisualStyleBackColor = true;
+            this.bdsc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // binc
+            // 
+            this.binc.Location = new System.Drawing.Point(51, 56);
+            this.binc.Name = "binc";
+            this.binc.Size = new System.Drawing.Size(13, 23);
+            this.binc.TabIndex = 14;
+            this.binc.Text = "+";
+            this.binc.UseVisualStyleBackColor = true;
+            this.binc.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // CaptureLabel
+            // 
+            this.CaptureLabel.BackColor = System.Drawing.Color.White;
+            this.CaptureLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CaptureLabel.Location = new System.Drawing.Point(9, 5);
+            this.CaptureLabel.Name = "CaptureLabel";
+            this.CaptureLabel.Size = new System.Drawing.Size(156, 48);
+            this.CaptureLabel.TabIndex = 16;
+            // 
+            // ReinoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(177, 120);
+            this.Controls.Add(this.bdsc);
+            this.Controls.Add(this.binc);
+            this.Controls.Add(this.cdsc);
+            this.Controls.Add(this.cinc);
+            this.Controls.Add(this.ddsc);
+            this.Controls.Add(this.dinc);
+            this.Controls.Add(this.adsc);
+            this.Controls.Add(this.ainc);
+            this.Controls.Add(this.bscore);
+            this.Controls.Add(this.cscore);
+            this.Controls.Add(this.dscore);
+            this.Controls.Add(this.ascore);
+            this.Controls.Add(this.bname);
+            this.Controls.Add(this.cname);
+            this.Controls.Add(this.dname);
+            this.Controls.Add(this.aname);
+            this.Controls.Add(this.CaptureLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ReinoForm";
+            this.Text = "かうんた";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label aname;
+        private System.Windows.Forms.Label dname;
+        private System.Windows.Forms.Label cname;
+        private System.Windows.Forms.Label bname;
+        private System.Windows.Forms.Label ascore;
+        private System.Windows.Forms.Label dscore;
+        private System.Windows.Forms.Label cscore;
+        private System.Windows.Forms.Label bscore;
+        private System.Windows.Forms.Button ainc;
+        private System.Windows.Forms.Button adsc;
+        private System.Windows.Forms.Button ddsc;
+        private System.Windows.Forms.Button dinc;
+        private System.Windows.Forms.Button cdsc;
+        private System.Windows.Forms.Button cinc;
+        private System.Windows.Forms.Button bdsc;
+        private System.Windows.Forms.Button binc;
+        private System.Windows.Forms.Label CaptureLabel;
+    }
+}
+
