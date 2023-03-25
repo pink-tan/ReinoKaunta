52bb3fcc9251713ab5e29cd219b971066dfb9e6b
pink-tan <pink.tan.tank@gmail.com>
2023-03-25 20:30:17 +0900

    Revert "Revert "GPT-4によるリファクタ""
    This reverts commit b30108c88ce178a1260e9e58dae7db4032bc2fd7.

--- ReinoKaunta/ReinoForm.cs
+++ ReinoKaunta/ReinoForm.cs
@@ -6,103 +6,70 @@ namespace ReinoKaunta
 {
     public partial class ReinoForm : Form
     {
-        /// <summary>
-        /// コントロールとスコアを管理するクラス
-        /// </summary>
-        private List<Controls> controls;
+        // コントロールとスコアを管理するクラスのリスト
+        private List<ControlSet> controlSets;
 
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
+        // コンストラクタ
         public ReinoForm()
         {
             InitializeComponent();
 
-            //コントロールとスコアを管理するクラスを初期化
-            controls = new List<Controls>(4);
-            controls.Add(new Controls(aname, ascore, ainc, adsc));
-            controls.Add(new Controls(bname, bscore, binc, bdsc));
-            controls.Add(new Controls(cname, cscore, cinc, cdsc));
-            controls.Add(new Controls(dname, dscore, dinc, ddsc));
+            // コントロールとスコアを管理するクラスのリストを初期化
+            controlSets = new List<ControlSet>(4);
+            controlSets.Add(new ControlSet(aname, ascore, ainc, adsc));
+            controlSets.Add(new ControlSet(bname, bscore, binc, bdsc));
+            controlSets.Add(new ControlSet(cname, cscore, cinc, cdsc));
+            controlSets.Add(new ControlSet(dname, dscore, dinc, ddsc));
         }
 
-        /// <summary>
-        /// ボタン押下共通メソッド
-        /// </summary>
-        /// <param name="sender">ボタン</param>
-        /// <param name="e"></param>
+        // ボタン押下共通メソッド
         private void buttonClick(object sender, EventArgs e)
         {
             // 押されたボタンを探す
-            foreach(var control in controls)
+            foreach (var controlSet in controlSets)
             {
                 // +ボタンが押されていた場合
-                if (control.IncButton.Equals(sender))
+                if (controlSet.IncButton.Equals(sender))
                 {
                     // 対応するスコアを加算して対応するスコアラベルに表示する
-                    control.Score++;
-                    control.ScoreLabel.Text = control.Score.ToString();
+                    controlSet.Score++;
+                    controlSet.ScoreLabel.Text = controlSet.Score.ToString();
                     break;
                 }
                 // -ボタンが押されていた場合
-                if (control.DscButton.Equals(sender))
+                if (controlSet.DscButton.Equals(sender))
                 {
-                    // 対応するスコアを減算して対応するスコアらゲルに表示する
-                    control.Score--;
-                    control.ScoreLabel.Text = control.Score.ToString();
+                    // 対応するスコアを減算して対応するスコアラベルに表示する
+                    controlSet.Score--;
+                    controlSet.ScoreLabel.Text = controlSet.Score.ToString();
                     break;
                 }
             }
         }
     }
 
-    /// <summary>
-    /// コントロールとスコアを管理するクラス
-    /// </summary>
-    public class Controls
+    // コントロールとスコアを管理するクラス
+    public class ControlSet
     {
-        /// <summary>
-        /// 名前ラベル
-        /// </summary>
         public Label NameLabel { get; set; }
-        /// <summary>
-        /// スコアラベル
-        /// </summary>
         public Label ScoreLabel { get; set; }
-        /// <summary>
-        /// +ボタン
-        /// </summary>
         public Button IncButton { get; set; }
-        /// <summary>
-        /// -ボタン
-        /// </summary>
         public Button DscButton { get; set; }
 
-        /// <summary>
-        /// スコア
-        /// </summary>
+        // スコア
         public int Score { get; set; }
 
-        /// <summary>
-        /// デフォコン隠蔽
-        /// </summary>
-        private Controls(){}
+        // デフォルトコンストラクタを隠蔽
+        private ControlSet() { }
 
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="namelabel">名前ラベル</param>
-        /// <param name="scorelabel">スコアラベル</param>
-        /// <param name="incbutton">+ボタン</param>
-        /// <param name="dscbutton">-ボタン</param>
-        public Controls(Label namelabel, Label scorelabel, Button incbutton, Button dscbutton)
+        // コンストラクタ
+        public ControlSet(Label nameLabel, Label scoreLabel, Button incButton, Button dscButton)
         {
-            NameLabel  = namelabel;
-            ScoreLabel = scorelabel;
-            IncButton  = incbutton;
-            DscButton  = dscbutton;
-            Score      = 0;
+            NameLabel = nameLabel;
+            ScoreLabel = scoreLabel;
+            IncButton = incButton;
+            DscButton = dscButton;
+            Score = 0;
         }
     }
-
 }
